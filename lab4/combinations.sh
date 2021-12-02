143684c1392bcb0f48820a81d03845082aba68d4
Parjanya Modi <71305818+parjanyamodi@users.noreply.github.com>
2021-12-02 01:05:27 -0500

    Update combinations.sh

--- lab4/combinations.sh
+++ lab4/combinations.sh
@@ -1,10 +1,11 @@
+echo "The combinations of 1, 2, & 3 are :"
 for i in 1 2 3
 do
-for j in 1 2 3 
-do
-for k in 1 2 3 
-do 
-echo "$i $j $k"
-done 
-done 
+  for j in 1 2 3 
+  do
+    for k in 1 2 3 
+    do 
+      echo "$i $j $k"
+    done 
+  done 
 done
